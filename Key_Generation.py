f09f0e1870e6ae04a23d037e01161cb19c61b27f
rinabekolli <rina.bekolli@student.uni-pr.edu>
2023-01-07 20:26:29 +0100

    Create Key_Generation.py
    initial commit

---
+++ Key_Generation.py
@@ -0,0 +1,21 @@
+# Import necessary modules from the cryptography library
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+# Generate a 4096-bit RSA private key with a public exponent of 65537
+private_key = rsa.generate_private_key(
+    public_exponent = 65537,
+    key_size = 4096,
+    backend = default_backend(),
+)
+
+# Save the private key to a file in PEM format
+with open('private.key', 'wb') as key_file:
+    key_file.write(
+        private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.NoEncryption(),
+        )
+    )
